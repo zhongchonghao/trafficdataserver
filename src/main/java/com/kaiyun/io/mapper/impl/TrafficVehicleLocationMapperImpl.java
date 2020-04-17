19bb230bc3cdb46dd48db8278a9119078ceb0d5e
仲崇浩 <zhong0608@163.com>
2020-04-17 12:49:02 +0800

    通过finally，确保session关闭

--- src/main/java/com/kaiyun/io/mapper/impl/TrafficVehicleLocationMapperImpl.java
+++ src/main/java/com/kaiyun/io/mapper/impl/TrafficVehicleLocationMapperImpl.java
@@ -16,16 +16,19 @@ public class TrafficVehicleLocationMapperImpl implements TrafficVehicleLocationM
     public String selectGPSNumByVehicleNo(String vehicleNo) {
         // 获取sqlSession
         SqlSession session = SqlSessionFactory.getSession();
+        String imei;
 
-        // 获取接口的实现类对象
-        // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
-        TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
+        try {
+            // 获取接口的实现类对象
+            // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
 
-        //调用代理对象方法
-        String imei = mapper.selectGPSNumByVehicleNo(vehicleNo);
-
-        // 关闭sqlSession
-        SqlSessionFactory.closeSession(session);
+            //调用代理对象方法
+            imei = mapper.selectGPSNumByVehicleNo(vehicleNo);
+        } finally {
+            // 关闭sqlSession
+            SqlSessionFactory.closeSession(session);
+        }
 
         return imei;
     }
@@ -34,19 +37,22 @@ public class TrafficVehicleLocationMapperImpl implements TrafficVehicleLocationM
     public int insertVehicleInfo(String code, String vehicleNo, String gpsNum) {
         // 获取sqlSession
         SqlSession session = SqlSessionFactory.getSession();
+        int insert;
 
-        // 获取接口的实现类对象
-        // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
-        TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
+        try {
+            // 获取接口的实现类对象
+            // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
 
-        //调用代理对象方法
-        int insert = mapper.insertVehicleInfo(code, vehicleNo, gpsNum);
+            //调用代理对象方法
+            insert = mapper.insertVehicleInfo(code, vehicleNo, gpsNum);
 
-        //如果做了更新、插入或删除操作，必须commit
-        session.commit();
-
-        // 关闭sqlSession
-        SqlSessionFactory.closeSession(session);
+            //如果做了更新、插入或删除操作，必须commit
+            session.commit();
+        } finally {
+            // 关闭sqlSession
+            SqlSessionFactory.closeSession(session);
+        }
 
         return insert;
     }
@@ -55,19 +61,22 @@ public class TrafficVehicleLocationMapperImpl implements TrafficVehicleLocationM
     public int insertTrack(TrafficVehicleLocationPO trafficVehicleLocationPO) {
         // 获取sqlSession
         SqlSession session = SqlSessionFactory.getSession();
+        int insert;
 
-        // 获取接口的实现类对象
-        // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
-        TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
+        try {
+            // 获取接口的实现类对象
+            // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
 
-        //调用代理对象方法
-        int insert = mapper.insertTrack(trafficVehicleLocationPO);
+            //调用代理对象方法
+            insert = mapper.insertTrack(trafficVehicleLocationPO);
 
-        //如果做了更新、插入或删除操作，必须commit
-        session.commit();
-
-        // 关闭sqlSession
-        SqlSessionFactory.closeSession(session);
+            //如果做了更新、插入或删除操作，必须commit
+            session.commit();
+        } finally {
+            // 关闭sqlSession
+            SqlSessionFactory.closeSession(session);
+        }
 
         return insert;
     }
@@ -77,15 +86,17 @@ public class TrafficVehicleLocationMapperImpl implements TrafficVehicleLocationM
         // 获取sqlSession
         SqlSession session = SqlSessionFactory.getSession();
 
-        // 获取接口的实现类对象
-        // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
-        TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
+        try {
+            // 获取接口的实现类对象
+            // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
 
-        //调用代理对象方法
-        imei = mapper.selectPosition(imei);
-
-        // 关闭sqlSession
-        SqlSessionFactory.closeSession(session);
+            //调用代理对象方法
+            imei = mapper.selectPosition(imei);
+        } finally {
+            // 关闭sqlSession
+            SqlSessionFactory.closeSession(session);
+        }
 
         return imei;
     }
@@ -94,19 +105,22 @@ public class TrafficVehicleLocationMapperImpl implements TrafficVehicleLocationM
     public int insertPosition(TrafficVehicleLocationPO trafficVehicleLocationPO) {
         // 获取sqlSession
         SqlSession session = SqlSessionFactory.getSession();
+        int insert;
 
-        // 获取接口的实现类对象
-        // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
-        TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
+        try {
+            // 获取接口的实现类对象
+            // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
 
-        //调用代理对象方法
-        int insert = mapper.insertPosition(trafficVehicleLocationPO);
+            //调用代理对象方法
+            insert = mapper.insertPosition(trafficVehicleLocationPO);
 
-        //如果做了更新、插入或删除操作，必须commit
-        session.commit();
-
-        // 关闭sqlSession
-        SqlSessionFactory.closeSession(session);
+            //如果做了更新、插入或删除操作，必须commit
+            session.commit();
+        } finally {
+            // 关闭sqlSession
+            SqlSessionFactory.closeSession(session);
+        }
 
         return insert;
     }
@@ -115,19 +129,22 @@ public class TrafficVehicleLocationMapperImpl implements TrafficVehicleLocationM
     public int updatePosition(TrafficVehicleLocationPO trafficVehicleLocationPO) {
         // 获取sqlSession
         SqlSession session = SqlSessionFactory.getSession();
+        int update;
 
-        // 获取接口的实现类对象
-        // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
-        TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
+        try {
+            // 获取接口的实现类对象
+            // 会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            TrafficVehicleLocationMapper mapper = session.getMapper(TrafficVehicleLocationMapper.class);
 
-        //调用代理对象方法
-        int update = mapper.updatePosition(trafficVehicleLocationPO);
+            //调用代理对象方法
+            update = mapper.updatePosition(trafficVehicleLocationPO);
 
-        //如果做了更新、插入或删除操作，必须commit
-        session.commit();
-
-        // 关闭sqlSession
-        SqlSessionFactory.closeSession(session);
+            //如果做了更新、插入或删除操作，必须commit
+            session.commit();
+        } finally {
+            // 关闭sqlSession
+            SqlSessionFactory.closeSession(session);
+        }
 
         return update;
     }
